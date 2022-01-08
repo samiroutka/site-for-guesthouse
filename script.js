edcaa8613c1a0ab829afa8371584e4fd415d7cf4
samiroutka <94228006+samiroutka@users.noreply.github.com>
2022-01-08 12:51:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const images = document.querySelectorAll('.slider .slider-line img')
+const sliderLine = document.querySelector('.slider-line')
+let count = 0
+let width
+
+function init () {
+    width = document.querySelector('.slider').offsetWidth
+    sliderLine.style.width = width * images.length + 'px'
+    images.forEach(item => {
+        item.style.width = width + 'px'
+        item.style.height = width*1,335616438356164 + 'px'
+    })
+
+    sliderLine.style.transform = 'translate(' + count*width + 'px)'
+}
+
+window.addEventListener('resize', init)
+init()
+
+
+document.querySelector('.left-btn').addEventListener('click', function(){
+    count++
+
+    if (count > 0) {
+        count = -2
+    }
+
+    sliderLine.style.transform = 'translate(' + count*width + 'px)'
+})
+
+document.querySelector('.right-btn').addEventListener('click', function(){
+    count--
+
+    if (count < -2) {
+        count = 0
+    }
+
+    sliderLine.style.transform = 'translate(' + count*width + 'px)'
+})
+
