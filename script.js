c6045e7d19d25ad87954ef04079a6026994f598d
samiroutka <94228006+samiroutka@users.noreply.github.com>
2022-01-09 17:55:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const images = document.querySelectorAll('.slider .slider-line img')
+const image = document.querySelector('.slider-line img')
 const sliderLine = document.querySelector('.slider-line')
 let count = 0
 let width
 
 function init () {
     width = document.querySelector('.slider').offsetWidth
+    
     sliderLine.style.width = width * images.length + 'px'
     images.forEach(item => {
         item.style.width = width + 'px'
