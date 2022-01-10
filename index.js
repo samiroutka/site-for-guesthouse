7659a32ff70522222d4861de0fdc9feba9cea7b4
samiroutka <94228006+samiroutka@users.noreply.github.com>
2022-01-10 18:22:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const imgs = document.querySelectorAll('.slider-line img')
+let img1 = document.querySelector('img:first-child')
+const slider = document.querySelector('.slider')
+const sliderLine = document.querySelector('.slider-line') 
+let forImgs = img1.offsetWidth/img1.offsetHeight
+let count = 0
+
+
+function start (){
+
+    sliderLine.style.width = slider.offsetWidth*imgs.length + 'px'
+
+    imgs.forEach(item =>{
+        item.style.width = slider.offsetWidth + 'px'
+        item.style.height = slider.offsetWidth/forImgs + 'px'
+    })
+}
+
+
+setInterval(function(){
+    count++
+
+    if (count > imgs.length-1){
+        count = 0
+    }
+
+    sliderLine.style.transform = 'translate(-' + slider.offsetWidth*count + 'px)'
+    console.log('slider.offsetWidth: ' + slider.offsetWidth)
+    console.log('slider.offsetWidth*count: ' + slider.offsetWidth*count)
+
+}, 1500)
+
+
+start()
+window.addEventListener('resize', start)
